22249d8bc0c05d3fb7f655041b16b44513c946d3
Lukeus Maximus <lm9131@bristol.ac.uk>
2012-10-30 15:20:01 +0000

    Added git ignore and work on scapy packets

--- scapy_stuff/test_listener.py
+++ scapy_stuff/test_listener.py
@@ -1,11 +1,30 @@
-import os, socket, select
+import os, socket, select, subprocess
 
-if __name__ == "__main__":
+def get_ip():
+    output_proc = subprocess.check_output(["ip", "addr"])
+    output_proc = output_proc.split("\n")
+    ips = []
+    for x in output_proc:
+        if x.find("    inet ") == 0:
+            ips.append(x[9:x.find("/")])
+    for x in ips:
+        if x != "127.0.0.1":
+            return x
+    return None
+
+def run_listener(ip):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind(('192.168.1.137', 1337)) ## You also specify a specific intf, like '255.255.255.255' for Bcast or '127.0.0.1' for localhost...
+    port = 1337
+    s.bind((ip, port)) ## You also specify a specific intf, like '255.255.255.255' for Bcast or '127.0.0.1' for localhost...
     s.setblocking(0)
 
     while True:
         result = select.select([s],[],[])
         msg = result[0][0].recv(1024) 
         print msg
+
+if __name__ == "__main__":
+    ip = get_ip()
+    if ip != None:
+        run_listener(ip)
+    
--- scapy_stuff/test_sender.py
+++ scapy_stuff/test_sender.py
@@ -1,17 +1,23 @@
 from time import sleep
+from test_listener import get_ip
 import os 
 import socket
 import sys
 
-if __name__ == "__main__":
+def run_sender(ip, interval):
     outsock =  socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     outsock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     bcast = "255.255.255.255"
     i = 0
     
     while True:
-        msg = "FOO_Master @ " + str(socket.gethostname()) + " " + str(i)
+        msg = "Luke @ " + str(socket.gethostname()) + " " + str(i)
         port = 1337
-        outsock.sendto(msg, ('192.168.1.137', port))
-        sleep(int(sys.argv[1]))
+        outsock.sendto(msg, (ip, port))
+        sleep(interval)
         i += 1
+
+if __name__ == "__main__":
+    ip = get_ip()
+    if ip != None and len(sys.argv) > 1:
+        run_sender(ip, int(sys.argv[1]))
