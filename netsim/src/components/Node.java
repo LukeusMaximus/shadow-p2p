25a0362065c1546c0b7428abc40b45c31fea0133
Luke Murray <github@lukemurray.co.uk>
2013-04-13 12:02:37 +0100

    Integrated virtual position map

--- netsim/src/components/Node.java
+++ netsim/src/components/Node.java
@@ -4,17 +4,16 @@ import java.awt.Color;
 import java.awt.Point;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.Map;
 import java.util.Queue;
 import java.util.UUID;
 
-import misc.VirtualPositionInfo;
-
-import packets.*;
-
+import packets.DummyPacket;
+import packets.JoinRequest;
+import packets.JoinResponse;
+import packets.Packet;
+import virtualposition.VirtualPositionMap;
 import crypto.KeySet;
 
 public class Node {
@@ -27,17 +26,13 @@ public class Node {
     private Color nodeColour;
     private Boolean inNetwork;
     private Queue<Packet> inBuffer;
-    private Integer networkWidth;
-    private VirtualPositionInfo localRoot;
-    private Map<Point, VirtualPositionInfo> pointsOwned;
+    private VirtualPositionMap virtPosMap;
     private KeySet keySet;
     
     public Node(PacketShepard internet) {
         this.nodeID = UUID.randomUUID();
         this.inBuffer = new LinkedList<Packet>();
-        this.networkWidth = 1;
-        this.localRoot = null;
-        this.pointsOwned = null;
+        this.virtPosMap = null;
         this.internet = internet;
         this.inNetwork = false;
         
@@ -67,17 +62,15 @@ public class Node {
     }
     
     public Integer getNetworkWidth() {
-        return networkWidth;
+        return virtPosMap.getNetworkWidth();
     }
     
     public boolean ownsPosition(Point nodePos) {
-        return nodePos.equals(localRoot.virtualPosition) || pointsOwned.keySet().contains(nodePos);
+        return virtPosMap.ownsPosition(nodePos);
     }
     
     public Collection<Point> getPositionsResponsible() {
-        Collection<Point> points = new HashSet<Point>(this.pointsOwned.keySet());
-        points.add(localRoot.virtualPosition);
-        return points;
+        return virtPosMap.listPointsOwned();
     }
 
     public void addMessageToInputBuffer(Packet packet) {
@@ -102,35 +95,18 @@ public class Node {
     }
     
     public void sendDummyPacket() {
-        DummyPacket packet = new DummyPacket(localRoot.east);
-        packet.setStartPosition(localRoot.virtualPosition);
-        Point pos = new Point(localRoot.virtualPosition);
-        pos = getEastPosition(pos);
-        packet.setEndPosition(pos);
-        internet.sendPacket(packet);
-        
-        packet = new DummyPacket(localRoot.north);
-        packet.setStartPosition(localRoot.virtualPosition);
-        pos = new Point(localRoot.virtualPosition);
-        pos = getNorthPosition(pos);
-        packet.setEndPosition(pos);
-        internet.sendPacket(packet);
-        
-        for(Point position : pointsOwned.keySet()) {
-            VirtualPositionInfo virtPosInfo = pointsOwned.get(position);
-            
-            packet = new DummyPacket(virtPosInfo.east);
-            packet.setStartPosition(virtPosInfo.virtualPosition);
-            pos = new Point(virtPosInfo.virtualPosition);
-            pos = getEastPosition(pos);
-            packet.setEndPosition(pos);
+        Collection<Point> points = virtPosMap.listPointsOwned();
+        for(Point p : points) {
+            Point eastPos = virtPosMap.getEastPosition(p);
+            DummyPacket packet = new DummyPacket(virtPosMap.getNodeIDFromPosition(eastPos));
+            packet.setStartPosition(p);
+            packet.setEndPosition(eastPos);
             internet.sendPacket(packet);
             
-            packet = new DummyPacket(virtPosInfo.north);
-            packet.setStartPosition(virtPosInfo.virtualPosition);
-            pos = new Point(virtPosInfo.virtualPosition);
-            pos = getNorthPosition(pos);
-            packet.setEndPosition(pos);
+            Point northPos = virtPosMap.getNorthPosition(p);
+            packet = new DummyPacket(virtPosMap.getNodeIDFromPosition(northPos));
+            packet.setStartPosition(p);
+            packet.setEndPosition(northPos);
             internet.sendPacket(packet);
         }
     }
@@ -138,72 +114,55 @@ public class Node {
     public void enterNetwork(Point pos, UUID north, UUID east) {
         System.out.println("Entering network at " + pos.toString());
         this.inNetwork = true;
-        VirtualPositionInfo virtPosInfo = new VirtualPositionInfo();
-        virtPosInfo.virtualPosition = pos;
-        virtPosInfo.east = east;
-        virtPosInfo.north = north;
-        this.localRoot = virtPosInfo;
-        this.networkWidth = 1;
-        while(this.networkWidth <= pos.x && this.networkWidth <= pos.y) {
-            this.networkWidth *= 2;
-        }
-        this.pointsOwned = new HashMap<Point, VirtualPositionInfo>();
+        this.virtPosMap = new VirtualPositionMap(this.nodeID, pos);
     }
     
     private void processPacket(Packet packet) {
         if(packet.getClass() == JoinRequest.class) {
+            System.out.println("Request received.");
             JoinRequest req = (JoinRequest)packet;
-            if(pointsOwned.isEmpty()) {
-                networkWidth = this.increaseNetworkSize();
-            }
-            VirtualPositionInfo position = pointsOwned.remove(pointsOwned.keySet().iterator().next());
             
-            // Send response
-            JoinResponse resp = new JoinResponse(req.getNodeID(), position.virtualPosition.x, position.virtualPosition.y,
-                    position.north, position.east);
-            internet.sendPacket(resp);
+            // Find appropriate position for the new node to join
+            Point pos = virtPosMap.getAvailableChildOnEdge();
+            
+            if(pos != null) {
+                System.out.println(pos.x + " " + pos.y);
+                // Send response
+                JoinResponse resp = new JoinResponse(req.getNodeID(), pos.x, pos.y,
+                        virtPosMap.getNodeIDFromPosition(virtPosMap.getNorthPosition(pos)),
+                        virtPosMap.getNodeIDFromPosition(virtPosMap.getEastPosition(pos)));
+                internet.sendPacket(resp);
+            } else {
+                System.out.println("pos is null");
+                UUID joiner = virtPosMap.findJoinerNode();
+                if(joiner != null) {
+                    System.out.println("joiner found with ID " + joiner.toString());
+                    // Send join request encapsulated to joiner
+                    
+                } else {
+                    System.out.println("increasing network size");
+                    // Increase network size
+                    virtPosMap.expandNetwork();
+                    // Get available position and perform ownership changes 
+                    pos = virtPosMap.getAvailableChildOnEdge();
+                    virtPosMap.changeOwnership(pos, req.getNodeID());
+                    // TODO certificate of ownership transfer
+                    // Send response
+                    JoinResponse resp = new JoinResponse(req.getNodeID(), pos.x, pos.y,
+                            virtPosMap.getNodeIDFromPosition(virtPosMap.getNorthPosition(pos)),
+                            virtPosMap.getNodeIDFromPosition(virtPosMap.getEastPosition(pos)));
+                    internet.sendPacket(resp);
+                }
+            }
         } else if(packet.getClass() == JoinResponse.class) {
             System.out.println("Response received.");
-            if(localRoot == null) {
+            if(!this.inNetwork) {
                 System.out.println("Response being processed.");
                 JoinResponse resp = (JoinResponse)packet;
                 Point joinPos = new Point(resp.getX(), resp.getY());
                 enterNetwork(joinPos, resp.getNorth(), resp.getEast());
             }
         }
-    }
-    
-    public Point getNorthPosition(Point pos) {
-        pos.y = (pos.y + 1) % networkWidth;
-        return pos;
-    }
-    
-    public Point getEastPosition(Point pos) {
-        pos.x = (pos.x + 1) % networkWidth;
-        return pos;
-    }
-    
-    public Point getSouthPosition(Point pos) {
-        pos.y = (pos.y - 1) % networkWidth;
-        return pos;
-    }
-    
-    public Point getWestPosition(Point pos) {
-        pos.x = (pos.x - 1) % networkWidth;
-        return pos;
-    }
-    
-    private Integer increaseNetworkSize() {
-        this.networkWidth *= 2;
-        Collection<Point> points = Node.getPointsInheritedForStepSize(localRoot.virtualPosition, networkWidth/2);
-        for(Point pos : points) {
-            VirtualPositionInfo virtPosInfo = new VirtualPositionInfo();
-            virtPosInfo.virtualPosition = pos;
-            virtPosInfo.east = localRoot.east;
-            virtPosInfo.north = localRoot.north;
-            pointsOwned.put(pos, virtPosInfo);
-        }
-        return this.networkWidth;
     }
     
     public static Integer getMaxWidth() {
@@ -248,14 +207,6 @@ public class Node {
             }
         }
         return inNetwork;
-    }
-    
-    private static Collection<Point> getPointsInheritedForStepSize(Point pos, Integer stepSize) {
-        ArrayList<Point> points = new ArrayList<Point>();
-        points.add(new Point(pos.x + stepSize, pos.y));
-        points.add(new Point(pos.x + stepSize, pos.y + stepSize));
-        points.add(new Point(pos.x, pos.y + stepSize));
-        return points;
     }
     
 }
